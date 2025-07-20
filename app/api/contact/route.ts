f11a4064938390fa68e8350b82b0f49dcaf5b16e
Aman Sheikh <haarishsheikh04@gmail.com>
2025-07-20 11:24:40 +0530

    Added a API route for POST req

---
+++ app/api/contact/route.ts
@@ -0,0 +1,51 @@
+import { Contact } from "@/models/Contact.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const { firstName, lastName, email, phoneNo, message, services, expense } =
+      body;
+
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !phoneNo ||
+      !message ||
+      !services ||
+      !expense
+    ) {
+      return NextResponse.json(
+        { error: "Missing or invalid required fields" },
+        { status: 400 }
+      );
+    }
+
+    const newContact = await Contact.create({
+      firstName,
+      lastName,
+      email,
+      phoneNo,
+      message,
+      services,
+      expense,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Contact created successfully",
+        contact: newContact,
+      },
+      { status: 201 }
+    );
+  } catch (err) {
+    console.error("Contact POST error:", err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
